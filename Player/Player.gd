c45c45d5207900b64a91c250e067a7c458bb951b
Joseph Hines <jthines3@gmail.com>
2021-09-08 19:56:00 -0400

    Basic bullets, need animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
-export (int) var move_speed = 200
+const Bullet : PackedScene = preload("res://Bullet/Bullet.tscn")
+
+export (int) var move_speed = 175
 export (int) var jump_speed = -420
 
 export (int) var gravity = 1500
 
-export (bool) var has_gun = false
+export (bool) var has_gun = true
 export (String) var idle_animation = "idle"
 export (String) var walk_animation = "walk"
 
@@ -13,13 +15,12 @@ var velocity = Vector2()
 var jumping = false
 var double_jumping = false
 var last_on_floor = 0
+var moving_direction = 1
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
-	_update_state()
-	
 	# Move left or right
 	velocity.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * move_speed
 	
@@ -44,33 +45,52 @@ func _physics_process(delta):
 	if (jumping or double_jumping) and is_on_floor():
 		jumping = false
 		double_jumping = false
+		
+	_update_state(velocity)
+	_animate(velocity)
+	_act(velocity)
 	
 	# Move, slide, and saved the resulting velocity
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	
-	_animate(velocity)
-
 # Ensure internal state is consistent before calculating physics
-func _update_state():
+func _update_state(velocity):
 	if has_gun:
 		idle_animation = "gun_idle"
 		walk_animation = "gun_walk"
 	else:
 		idle_animation = "idle"
 		walk_animation = "walk"
+		
+	if velocity.x > 0:
+		moving_direction = 1
+	elif velocity.x < 0:
+		moving_direction = -1
+	
 	
 # Apply animations	
 func _animate(velocity):
-  # if is_on_floor():
 	if velocity.x == 0:
 		$AnimatedSprite.animation = idle_animation
 		$AnimatedSprite.stop()
-	elif velocity.x > 0:
-		$AnimatedSprite.animation = walk_animation
+	elif moving_direction == 1:
 		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play()
-	elif velocity.x < 0:
 		$AnimatedSprite.animation = walk_animation
+		$AnimatedSprite.play()
+	elif moving_direction == -1:
 		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.animation = walk_animation
 		$AnimatedSprite.play()
 	
+func _act(velocity):
+	if has_gun and Input.is_action_just_pressed("ui_accept"):
+		var bullet_position = Vector2(position.x + (moving_direction*7), position.y+3)
+		
+		var bullet = Bullet.instance()
+		bullet.creator_id = get_instance_id()
+		bullet.position = bullet_position
+		bullet.scale *= 0.5
+		
+		if moving_direction == -1:
+			bullet.rotation = PI
+		owner.add_child(bullet)
